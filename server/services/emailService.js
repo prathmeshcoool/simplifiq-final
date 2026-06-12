a11a7c4a82fa81efbfbf5f73257d4cc94dc4365d
prathmeshcool <prathmeshnaiknaware1@gmail.com>
2026-06-12 18:00:56 +0530

    Update email service to use port 587 and STARTTLS

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -3,11 +3,17 @@ const { cleanupPDF } = require("./pdfService");
 
 const sendEmail = async (to, company, pdfPath) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,       // STARTTLS, not SSL
+    requireTLS: true,    // force TLS upgrade
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   const info = await transporter.sendMail({
